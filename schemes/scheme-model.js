43cd0ddfd9dd7b494c16e31b3821cd0910df9fb8
lee-wood <leemaewood@gmail.com>
2020-02-04 23:43:14 -0500

    get all schemes endpoint

---
+++ schemes/scheme-model.js
@@ -0,0 +1,36 @@
+const db = require('../data/db-config.js');
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+
+}
+
+function findSteps(id) {
+
+}
+
+function add(scheme) {
+
+}
+
+function update(changes, id) {
+
+}
+
+function remove(id) {
+
+}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+}
+
+
